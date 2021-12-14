e496817f60ba48fb73baa08bc9b9cdef8c6161a7
Katarzyna Kawala <kat.kawala@gmail.com>
2021-12-14 22:23:51 +0100

    inicialization of server and dependency injection of application

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,7 +8,7 @@ import (
 	"text/template"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -22,19 +22,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
 	}
 
 	err = ts.Execute(w, nil)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 	}
 }
 
-func displayCoffee(w http.ResponseWriter, r *http.Request) {
+func (app *application) displayCoffee(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -44,7 +44,7 @@ func displayCoffee(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Display a specific coffee with ID %d", id)
 }
 
-func createCoffee(w http.ResponseWriter, r *http.Request) {
+func (app *application) createCoffee(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost) 
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,22 +7,38 @@ import (
 	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
-    addr := flag.String("addr", ":4000", "HTTP network address")
-    flag.Parse()
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
 
-    infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-    errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-    mux := http.NewServeMux()
-    mux.HandleFunc("/", home)
-    mux.HandleFunc("/coffee", displayCoffee)
-    mux.HandleFunc("/coffee/create", createCoffee)
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
 
-    fileServer := http.FileServer(http.Dir("./ui/static/"))
-    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/coffee", app.displayCoffee)
+	mux.HandleFunc("/coffee/create", app.createCoffee)
 
-    infoLog.Printf("Starting server on %s", *addr)
-    err := http.ListenAndServe(*addr, mux)
-    errorLog.Fatal(err)
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
