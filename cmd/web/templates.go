354193a8e51ce9afc012c9d1ec96113a434e3bcc
Katarzyna Kawala <kat.kawala@gmail.com>
2021-12-22 09:46:35 +0100

    adding readable date function

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -4,12 +4,21 @@ import (
 	"katarzynakawala/github.com/coffee-shop/pkg/models"
 	"path/filepath"
 	"text/template"
+	"time"
 )
 
 type templateData struct {
 	CurrentYear int
 	Coffee      *models.Coffee
 	Coffees     []*models.Coffee
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
@@ -23,7 +32,7 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
