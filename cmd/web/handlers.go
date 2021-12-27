3867a47379788766f5885ff216ab58a31893147d
Katarzyna Kawala <kat.kawala@gmail.com>
2021-12-27 14:38:31 +0100

    changing routes rules according to pat

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,11 +9,7 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
-
+	
 	c, err := app.coffees.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -26,7 +22,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) displayCoffee(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -47,12 +43,11 @@ app.render(w, r, "display.page.tmpl", &templateData{
 	})	
 }
 
+func (app *application) createCofffeeForm(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new coffee"))
+}
+
 func (app *application) createCoffee(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost) 
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
 
 	name := "black coffee"
 	ingredients := "water coffee"
@@ -63,5 +58,5 @@ func (app *application) createCoffee(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/coffee?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/coffee/id=%d", id), http.StatusSeeOther)
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,19 +3,20 @@ package main
 import (
 	"net/http"
 
+	"github.com/bmizerany/pat"
 	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
 	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/coffee", app.displayCoffee)
-	mux.HandleFunc("/coffee/create", app.createCoffee)
+	mux := pat.New()
+	mux.Get("/", http.HandlerFunc(app.home))
+	mux.Post("/coffee/create", http.HandlerFunc(app.createCoffee))
+	mux.Get("/coffee/:id", http.HandlerFunc(app.displayCoffee))
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.Get("/static/", http.StripPrefix("/static", fileServer))
 
 	return standardMiddleware.Then(mux)
 }
