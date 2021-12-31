889667ce5d271e8be4969f9a6ad45680fdb8831d
Katarzyna Kawala <kat.kawala@gmail.com>
2021-12-31 09:57:12 +0100

    adding e2e tests for handler

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"testing"
 )
@@ -18,5 +19,41 @@ func TestPing(t *testing.T) {
 
 	if string(body) != "OK" {
 		t.Errorf("want body to equal %q", "OK")
+	}
+}
+
+func TestDisplayCoffee(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	tests := []struct {
+		name string
+		urlPath string
+		wantCode int
+		wantBody []byte
+	}{
+		{"Valid ID", "/coffee/1", http.StatusOK, []byte("Coffee")},
+		{"Non-existent ID", "/coffee/2", http.StatusNotFound, nil},
+        {"Negative ID", "/coffee/-1", http.StatusNotFound, nil},
+        {"Decimal ID", "/coffee/1.23", http.StatusNotFound, nil},
+        {"String ID", "/coffee/foo", http.StatusNotFound, nil},
+        {"Empty ID", "/coffee/", http.StatusNotFound, nil},
+        {"Trailing slash", "/coffee/1/", http.StatusNotFound, nil},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			code, _, body := ts.get(t, tt.urlPath)
+
+			if code != tt.wantCode {
+				t.Errorf("want %d; got %d", tt.wantCode, code)
+			}
+
+			if !bytes.Contains(body, tt.wantBody) {
+				t.Errorf("want body to contains %q", tt.wantBody)
+			}
+		})
 	}
 }
